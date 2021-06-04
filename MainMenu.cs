42de9edfce7250027ba67d9c3104be037eda2900
Koro234 <tra.ne555222@yandex.ru>
2021-06-04 17:26:05 +0400

    added MainMenu

---
+++ MainMenu.cs
@@ -0,0 +1,19 @@
+using Godot;
+using System;
+
+public class MainMenu : Control
+{
+    [Export]
+    public PackedScene ClientScene;
+    [Export]
+    public PackedScene ServerScene;
+    public void _on_ClientButton_pressed()
+    {
+        GetTree().ChangeSceneTo(ClientScene);
+    }
+
+    public void _on_ServerButton_pressed()
+    {
+        GetTree().ChangeSceneTo(ServerScene);
+    }
+}
