ce3d6410e6d95ca501ba6810c6f88cd9228e6f7a
Koro234 <tra.ne555222@mail.ru>
2021-07-05 12:32:18 +0400

    Added Global

---
+++ Global.cs
@@ -0,0 +1,12 @@
+using Godot;
+using System;
+
+public class Global
+{
+    public string UserName {get; set;} = "User";
+    public string ServerName {get; set;} = "Server";
+    public string Ip {get; set;} = "";
+    public int Port {get; set;} = 0;
+    public int Quantity {get; set;} = 0;
+    
+}
--- MainMenu.cs
+++ MainMenu.cs
@@ -3,17 +3,17 @@ using System;
 
 public class MainMenu : Control
 {
-    [Export]
-    public PackedScene ClientScene;
-    [Export]
-    public PackedScene ServerScene;
-    public void _on_ClientButton_pressed()
-    {
-        GetTree().ChangeSceneTo(ClientScene);
-    }
+	[Export]
+	public PackedScene ClientScene;
+	[Export]
+	public PackedScene ServerScene;
+	public void _on_ClientButton_pressed()
+	{
+		GetTree().ChangeSceneTo(ClientScene);
+	}
 
-    public void _on_ServerButton_pressed()
-    {
-        GetTree().ChangeSceneTo(ServerScene);
-    }
+	public void _on_ServerButton_pressed()
+	{
+		GetTree().ChangeSceneTo(ServerScene);
+	}
 }
